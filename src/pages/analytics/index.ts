98b847fd495dd3337663773e776fe55a03fd5614
Ciaran Finnegan <ciaran.finnegan@cmd.com.au>
2024-08-04 22:21:24 +0100

    added analytics page

---
+++ src/pages/analytics/index.ts
@@ -0,0 +1,6 @@
+export { default as Analytics } from './analytics';
+export * from "./analytics"; 
+// export * from "./create";
+// export * from "./edit";
+// export * from "./list";
+// export * from "./show";

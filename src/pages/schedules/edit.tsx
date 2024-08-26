0f5604e6957ab048611a229fbbc99070cc1bf35d
Ciaran Finnegan <ciaran.finnegan@cmd.com.au>
2024-08-26 13:22:57 +0100

    and again

--- src/pages/schedules/edit.tsx
+++ src/pages/schedules/edit.tsx
@@ -1,68 +1,58 @@
 import { Edit, useForm } from "@refinedev/antd";
-import { Form, Input } from "antd";
+import { Form, Input, TimePicker } from "antd";
+import moment, { Moment } from "moment";
 
-export const PlatesEdit = () => {
-  const { formProps, saveButtonProps } = useForm({});
+interface ScheduleFormData {
+    day_of_week: string;
+    start_time: Moment | null; // Using moment type for time-related fields
+    end_time: Moment | null;
+}
 
-  return (
-    <Edit saveButtonProps={saveButtonProps}>
-      <Form {...formProps} layout="vertical">
-        <Form.Item
-          label={"Plate"}
-          name={["plate"]}
-          rules={[
-            {
-              required: true,
+export const SchedulesEdit = () => {
+    const { formProps, saveButtonProps } = useForm<ScheduleFormData>({
+        metaData: {
+            onLoad: (data: ScheduleFormData) => {
+                return {
+                    ...data,
+                    start_time: data.start_time ? moment(data.start_time, "HH:mm:ss") : null,
+                    end_time: data.end_time ? moment(data.end_time, "HH:mm:ss") : null,
+                };
             },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label={"Name"}
-          name={["name"]}
-          rules={[
-            {
-              required: true,
+            transform: (values: ScheduleFormData) => {
+                return {
+                    ...values,
+                    start_time: values.start_time ? values.start_time.format("HH:mm:ss") : null,
+                    end_time: values.end_time ? values.end_time.format("HH:mm:ss") : null,
+                };
             },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label={"Colour"}
-          name={["colour"]}
-          rules={[
-            {
-              required: true,
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label={"Make"}
-          name={["make"]}
-          rules={[
-            {
-              required: true,
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label={"Model"}
-          name={["model"]}
-          rules={[
-            {
-              required: true,
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-      </Form>
-    </Edit>
-  );
+        },
+    });
+
+    return (
+        <Edit saveButtonProps={saveButtonProps}>
+            <Form {...formProps} layout="vertical">
+                <Form.Item
+                    label="Day of Week"
+                    name="day_of_week"
+                    rules={[{ required: true }]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Start Time"
+                    name="start_time"
+                    rules={[{ required: true }]}
+                >
+                    <TimePicker format="HH:mm:ss" />
+                </Form.Item>
+                <Form.Item
+                    label="End Time"
+                    name="end_time"
+                    rules={[{ required: true }]}
+                >
+                    <TimePicker format="HH:mm:ss" />
+                </Form.Item>
+            </Form>
+        </Edit>
+    );
 };
